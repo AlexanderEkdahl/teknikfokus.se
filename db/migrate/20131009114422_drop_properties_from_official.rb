274f8a37695978f1551d58cefadcc0293158d94c
Alexander Ekdahl <alexander@ekdahl.io>
2013-10-09 14:15:56 +0200

    Properly removed migrations

--- db/migrate/20131009114422_drop_properties_from_official.rb
+++
@@ -1,5 +0,0 @@
-class DropPropertiesFromOfficial < ActiveRecord::Migration
-  def change
-    # remove_column :officials, :properties
-  end
-end
--- db/migrate/20131009114550_remove_hstore_extension.rb
+++
@@ -1,9 +0,0 @@
-class RemoveHstoreExtension < ActiveRecord::Migration
-  def up
-    # execute 'DROP EXTENSION hstore'
-  end
-
-  def down
-    # execute 'CREATE EXTENSION hstore'
-  end
-end
